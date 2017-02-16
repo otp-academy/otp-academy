2252199ca9dd7532fe7f888537654e6e63b09c31
Jonathan J Rim <jonrim@umich.edu>
2017-02-16 15:42:56 -0500

    request handler for latest league version

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,19 @@ var apiKey = require('../../secrets').DEV_API;
 module.exports = router;
 
 router.get('/', (req, res, next) => {
+	var leagueVersionsURL = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key='+apiKey;
+	// Get latest league version
+	// Use latest league version to get champion images
+	request(leagueVersionsURL, (error, response, body) => {
+		if (error) {
+			console.log("Error on GET request for Riot's league versions list");
+		}
+		else {
+			res.send(body[0]);
+		}
+	});
 
-	var championIDsURL = 'https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key='+apiKey;
+	var championIDsURL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key='+apiKey;
 	// Get list of champion IDs
 	request(championIDsURL, (error, response, body) => {
 		if (error) {
@@ -16,6 +27,7 @@ router.get('/', (req, res, next) => {
 			console.log(body);
 		}
 	});
+
 
 });
 
