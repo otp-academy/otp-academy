71448439e0322e571c1d7fe3bebe106c0803d568
Jonathan J Rim <jonrim@umich.edu>
2017-02-16 19:39:49 -0500

    fixed route for getting latest league version

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var request = require('request');
 var apiKey = require('../../secrets').DEV_API;
 module.exports = router;
 
-router.get('/', (req, res, next) => {
+router.get('/version', (req, res, next) => {
 	var leagueVersionsURL = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key='+apiKey;
 	// Get latest league version
 	// Use latest league version to get champion images
@@ -12,23 +12,30 @@ router.get('/', (req, res, next) => {
 			console.log("Error on GET request for Riot's league versions list");
 		}
 		else {
-			res.send(body[0]);
+			// Parse for latest version
+			var currentVersion = body.split(',')[0];
+			// Remove the Array char '['
+			currentVersion = currentVersion.slice(1, currentVersion.length);
+			// Remove excess quotes
+			currentVersion = currentVersion.replace(/"/g, '');
+			res.send(currentVersion);
 		}
 	});
+});
 
-	var championIDsURL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key='+apiKey;
+
+router.get('/champions', (req, res, next) => {
+	var championsURL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key='+apiKey;
 	// Get list of champion IDs
-	request(championIDsURL, (error, response, body) => {
+	request(championsURL, (error, response, body) => {
 		if (error) {
-			console.log("Error on GET request for Riot's champion IDs list");
+			console.log("Error on GET request for Riot's champion list");
 		}
 		else {
 			res.json(body);
 			console.log(body);
 		}
 	});
-
-
 });
 
 router.use((req, res) => {
