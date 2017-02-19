10300bcca855c822c561e2858d84c78f642d4d8e
coldshower <john@johnchen.co>
2017-02-18 23:01:27 -0500

    modified auth reducer

--- client/src/reducers/auth/index.js
+++ client/src/reducers/auth/index.js
@@ -5,16 +5,24 @@ import signUp from './signUp';
 import * as Consts from '../../constants/auth';
 
 const initialState = {
-  username: ''
+  user: null
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case Consts.LOGIN_SUCCESS:
     case Consts.SIGN_UP_SUCCESS:
+    case Consts.SESSION_SUCCESS:
       return {
         ...state,
-        username: action.user.username
+        user: action.result.user
+      };
+    case Consts.LOGIN_FAILED:
+    case Consts.SIGN_UP_FAILED:
+    case Consts.SESSION_FAILED:
+      return {
+        ...state,
+        user: null
       };
     default:
       return state;
