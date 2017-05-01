5bb3e01df7cc16c2068d42cb049598ea87d1021f
coldshower <john@johnchen.co>
2017-04-30 21:06:22 -0400

    add sass to project

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -27,6 +27,10 @@ module.exports = {
         loader: 'style!css'
       },
       {
+        test: /\.scss$/,
+        loader: ['style', 'css', 'sass']
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
             'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
